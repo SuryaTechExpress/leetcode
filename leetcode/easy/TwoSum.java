e28b9d0e362b0578370d27f6dca8a35e3a1ecded
SuryaTechExpress <surajkumarsah999@gmail.com>
2024-01-26 12:37:25 +0530

    first commit

--- leetcode/easy/TwoSum.java
+++ leetcode/easy/TwoSum.java
@@ -1,7 +1,10 @@
 package leetcode.easy;
 
+import java.util.Arrays;
+
 public class TwoSum {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] array = {4, 3, 1, 6, 8, 5, 3, 2, 3, 5, 6};
+        System.out.println(Arrays.toString(array));
     }
 }
