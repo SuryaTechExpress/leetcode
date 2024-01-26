914b7b03495a7e70af28a1d2c9d2134a1cfd84bd
SuryaTechExpress <surajkumarsah999@gmail.com>
2024-01-26 13:57:22 +0530

    two sum program leetcode using map

--- leetcode/easy/TwoSum.java
+++ leetcode/easy/TwoSum.java
@@ -1,10 +1,28 @@
 package leetcode.easy;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] array = {4, 3, 1, 6, 8, 5, 3, 2, 3, 5, 6};
-        System.out.println(Arrays.toString(array));
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(toSum(nums,target)));
+    }
+    public static int[] toSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        // Build the hash table
+        for(int i=0;i<n;i++){
+            map.put(nums[i],i);
+        }
+        // Find the complement
+        for (int i=0;i<n;i++){
+            int complement = target-nums[i];
+            if(map.containsKey(complement) && map.get(complement)!=i);
+            return new int[]{i,map.get(complement)};
+        }
+        return new int[]{};
     }
 }
