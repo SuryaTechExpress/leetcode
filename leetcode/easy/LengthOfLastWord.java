a4812d7a9ecd4b02d3789021fd03dda0fb7ce896
SuryaTechExpress <surajkumarsah999@gmail.com>
2024-01-28 15:08:10 +0530

    08 leetcode Add Binary

--- leetcode/easy/LengthOfLastWord.java
+++ leetcode/easy/LengthOfLastWord.java
@@ -1,13 +1,16 @@
 package leetcode.easy;
 
 public class LengthOfLastWord {
+
     public static void main(String[] args) {
        String s = "Hello World";
         System.out.println(lengthOfLastWord(s));
     }
+
     public static int lengthOfLastWord(String s){
         String[] array = s.trim().split(" ");
         String lastElement = array[array.length-1];
         return lastElement.length();
     }
+
 }
