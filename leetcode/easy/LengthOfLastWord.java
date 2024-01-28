8191a149c3e75f3893c752de47c2928ac310452c
SuryaTechExpress <surajkumarsah999@gmail.com>
2024-01-28 14:56:46 +0530

    08 leetcode Length of last word

---
+++ leetcode/easy/LengthOfLastWord.java
@@ -0,0 +1,13 @@
+package leetcode.easy;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+       String s = "Hello World";
+        System.out.println(lengthOfLastWord(s));
+    }
+    public static int lengthOfLastWord(String s){
+        String[] array = s.trim().split(" ");
+        String lastElement = array[array.length-1];
+        return lastElement.length();
+    }
+}
